555afff65aecaa0340781c17d5e0189d22f2bfd3
dawidolko <dawid_olko@outlook.com>
2025-03-02 15:22:43 +0100

    Akutalizacja struktury drzewa projektu. Dodanie niezbędnych poprawek GUI, udokumentowanie wstępnych zmian (panelu admina, klienta, admina) oraz (panelu logowania, rejestracji, ustawień). Utworzenie plików rozruchowych na sytemy linux/windows.

---
+++ archive.bat
@@ -0,0 +1,7 @@
+git init
+git config --local user.name "dawidolko"
+git config --local user.email "do125148@stud.ur.edu.pl"
+git add --all
+git commit -m "TaskApp"
+git archive --format=zip HEAD -o ../TaspApp.zip
+pause
